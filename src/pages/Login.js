4d68264616f7206b7eb178f9cddfa91d4108727e
Carlo <carlo.panzi@gmail.com>
2020-05-03 16:13:52 +0200

    feat: Initial login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,51 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 const Container = styled.div`
   display: flex;
 `;
 
-const Login = ({ aside, children }) => {
+const Login = ({ onSubmit, onResetPassword }) => {
+  const { register, handleSubmit } = useForm();
   return (
-    <div className="animate form login_form">
-      <section className="login_content">
-        <form>
-          <h1>Login Form</h1>
+    <section className="login_content">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Login Form</h1>
 
-          <div>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Username"
-              required=""
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Password"
-              required=""
-            />
-          </div>
+        <div>
+          <input type="email" placeholder="Email" name="email" ref={register} />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            ref={register}
+          />
+        </div>
 
-          <div>
-            <a className="btn btn-default submit" href="index.html">
-              Log in
-            </a>
-            <a className="reset_pass" href="#">
-              Lost your password?
-            </a>
-          </div>
-          <p className="change_link">
-            New to site?
-            <a href="#signup" className="to_register">
-              Create Account
-            </a>
-          </p>
-        </form>
-      </section>
-    </div>
+        <div>
+          <button className="btn btn-default submit" href="index.html">
+            Log in
+          </button>
+          {onResetPassword && (
+            <button onClick={onResetPassword}>Lost your password?</button>
+          )}
+        </div>
+      </form>
+    </section>
   );
 };
 
